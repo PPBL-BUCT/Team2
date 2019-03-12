6a43e187f7fd6dc904ca9bd8b9f5c184f58cea57
Maosansan <18611376595@163.com>
2019-03-12 15:34:16 +0800

    update

---
+++ Week01/Client/ClientThread.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+/**
+ * 输出信息线程
+ */
+public class ClientThread implements Runnable {
+	// 定义一个DataInputStream变量
+	DataInputStream di;
+	// 定义一个string对象接收服务器发送的消息
+	String str;
+
+	@Override
+	public void run() {
+
+	}
+}
+ 
