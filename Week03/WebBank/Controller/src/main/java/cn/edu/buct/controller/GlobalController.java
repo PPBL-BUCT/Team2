a86debe12b28c921e752356c1494f75b1b5dfa0f
Maosansan <18611376595@163.com>
2019-03-28 17:59:39 +0800

    修改配置,添加拦截器和过滤器

--- Week03/WebBank/Controller/src/main/java/cn/edu/buct/controller/GlobalController.java
+++ Week03/WebBank/Controller/src/main/java/cn/edu/buct/controller/GlobalController.java
@@ -1,4 +1,11 @@
 package cn.edu.buct.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller("globalController")
 public class GlobalController {
+    @RequestMapping("/to_login")
+    public String toLogin(){
+        return "loginUI";
+    }
 }
---
+++ Week03/WebBank/Controller/src/main/java/cn/edu/buct/global/LoginInterceptor.java
@@ -0,0 +1,38 @@
+package cn.edu.buct.global;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+
+        String url = httpServletRequest.getRequestURI();
+        if(url.toLowerCase().indexOf("login")>=0)
+        {
+            //找到login
+            return true;
+        }
+        HttpSession session = httpServletRequest.getSession();
+        if(session.getAttribute("user")!=null)
+        {
+            return true;
+        }
+        httpServletResponse.sendRedirect("/to_login");
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
